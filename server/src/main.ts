a57064b8bab4edcbd9d7d1e65db57c449d84e932
billal12356 <boudjellbillal@gmail.com>
2025-04-15 04:44:44 +0200

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1')
   app.enableCors({
-    origin: "http://localhost:5173",  
+    origin: "[http://localhost:5173,https://trensporteur-company.vercel.app]",  
     credentials: true,  
   });
   app.useGlobalPipes(new ValidationPipe());
