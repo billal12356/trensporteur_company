f56b71b7069677f4b36480cd34cff61dd7a16a01
billal12356 <boudjellbillal@gmail.com>
2025-10-09 15:13:59 +0100

    fixe pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,34 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1')
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://trensporteur-company.vercel.app',
-    ],
-    credentials: true,
+    origin: ['http://localhost:5173', 'https://trensporteur-company.vercel.app'],  
+    credentials: true,  
   });
 
   // const seeder = app.get(ImportOperateurService);
   // await seeder.run();
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // يحذف الحقول غير الموجودة في DTO
-      forbidNonWhitelisted: true,
-      transform: true,
-      exceptionFactory: (errors) => {
-        // نخصّص شكل رسالة الخطأ
-        const messages = errors.map(
-          (err) => `${Object.values(err.constraints ?? {}).join(', ')}`,
-        );
-        return new Error(messages.join(' | '));
-      },
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
